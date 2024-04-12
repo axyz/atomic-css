c74293c81a50e9be14ad7d83a5467f6fe11edb76
Andrea Moretti <axyzxp@gmail.com>
2024-04-12 11:02:11 +0200

    test: parser

--- atomic-css-parser/src/lexer.rs
+++ atomic-css-parser/src/lexer.rs
@@ -11,7 +11,7 @@ pub enum Token {
     #[token(")")]
     RParen,
 
-    #[regex("[&@a-zA-Z][a-zA-z0-9-_]*", |lex| lex.slice().to_owned())]
+    #[regex("[&@a-zA-Z][a-zA-Z0-9-_]*", |lex| lex.slice().to_owned())]
     Identifier(String),
 
     #[regex(r#"`([^`])*`"#, |lex| lex.slice()[1..lex.slice().len() - 1].to_owned())]
@@ -21,26 +21,49 @@ pub enum Token {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use logos::Logos;
 
     #[test]
-    fn it_lexes() {
-        let lex = Token::lexer(
-            r#"
-; comment
-(molecule `flag` ;;another comment
-  (atom `root`)
-  (atom `label` (import `button/label`))
-  ;yet another comment
-  (rule `${root}` (padding `1rem`))
-  (@foo)
-  (@bar `baz`)
-  (@media `(min-width: 1024px)`
-    (rule `${root}` (padding `1.5rem`))))
-"#,
-        );
+    fn test_token_lparen() {
+        let mut lexer = Token::lexer("(");
+        assert_eq!(lexer.next(), Some(Ok(Token::LParen)));
+    }
 
-        for token in lex {
-            //println!("{:?}", token)
-        }
+    #[test]
+    fn test_token_rparen() {
+        let mut lexer = Token::lexer(")");
+        assert_eq!(lexer.next(), Some(Ok(Token::RParen)));
+    }
+
+    #[test]
+    fn test_token_identifier() {
+        let mut lexer = Token::lexer("identifier");
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("identifier".to_string())))
+        );
+    }
+
+    #[test]
+    fn test_token_string() {
+        let mut lexer = Token::lexer("`string`");
+        assert_eq!(lexer.next(), Some(Ok(Token::String("string".to_string()))));
+    }
+
+    #[test]
+    fn test_token_complex() {
+        let mut lexer = Token::lexer("(identifier1 `string1` identifier2 `string2`)");
+        assert_eq!(lexer.next(), Some(Ok(Token::LParen)));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("identifier1".to_string())))
+        );
+        assert_eq!(lexer.next(), Some(Ok(Token::String("string1".to_string()))));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("identifier2".to_string())))
+        );
+        assert_eq!(lexer.next(), Some(Ok(Token::String("string2".to_string()))));
+        assert_eq!(lexer.next(), Some(Ok(Token::RParen)));
     }
 }
--- atomic-css-parser/src/parser.rs
+++ atomic-css-parser/src/parser.rs
@@ -82,23 +82,144 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_function_valid() {
+        let result = parse("(myFunc arg1 arg2)");
+        assert_eq!(
+            result,
+            Ok(vec![Node::Function(
+                "myFunc".to_string(),
+                vec![
+                    Node::Identifier("arg1".to_string()),
+                    Node::Identifier("arg2".to_string())
+                ]
+            )])
+        );
+    }
+
+    #[test]
+    fn test_parse_function_invalid() {
+        let result = parse("(1234)");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_function_unmatched() {
+        let result = parse("(myFunc arg1 arg2");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_empty() {
+        let result = parse("");
+        assert_eq!(result, Ok(vec![]));
+    }
+
+    #[test]
+    fn test_parse_empty_function_invalid() {
+        let result = parse("()");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_non_function() {
+        let number = parse("1234");
+        assert!(number.is_err());
+
+        let string = parse("`string`");
+        assert!(string.is_err());
+
+        let identifier = parse("identifier");
+        assert!(identifier.is_err());
+    }
+
+    #[test]
+    fn test_node_string_valid() {
+        let result = parse("(test `valid string`)");
+        assert_eq!(
+            result,
+            Ok(vec![Node::Function(
+                "test".to_string(),
+                vec![Node::String("valid string".to_string())]
+            )])
+        );
+    }
+
+    #[test]
+    fn test_node_string_invalid() {
+        let result = parse("(test invalid string)");
+        assert_ne!(
+            result,
+            Ok(vec![Node::Function(
+                "test".to_string(),
+                vec![Node::String("invalid string".to_string())]
+            )])
+        );
+    }
+
+    #[test]
+    fn test_node_identifier_valid() {
+        let result = parse("(validIdentifier)");
+        assert_eq!(
+            result,
+            Ok(vec![Node::Function("validIdentifier".to_string(), vec![])])
+        );
+    }
+
+    #[test]
+    fn test_node_identifier_dash() {
+        let result = parse("(valid-identifier)");
+        assert_eq!(
+            result,
+            Ok(vec![Node::Function("valid-identifier".to_string(), vec![])])
+        );
+    }
+
+    #[test]
+    fn test_node_identifier_at() {
+        let result = parse("(@identifier)");
+        assert_eq!(
+            result,
+            Ok(vec![Node::Function("@identifier".to_string(), vec![])])
+        );
+    }
+
+    #[test]
+    fn test_node_identifier_ampersand() {
+        let result = parse("(&)");
+        assert_eq!(result, Ok(vec![Node::Function("&".to_string(), vec![])]));
+    }
+
+    #[test]
+    fn test_node_identifier_invalid() {
+        let result = parse("(1234)");
+        assert_ne!(result, Ok(vec![Node::Function("1234".to_string(), vec![])]));
+    }
+
+    #[test]
     fn it_parses() {
         let source = r##"
-(electron `bg_green` (background-color `#00ff00`))
-; comment
 (electron `red` (color `#ff0000`))
-(organism `test` ;comment
-  (molecule `flag`
-    (atom `root`)
-    (atom `label` (import `button.label`))
-    (rule `${root}` (padding `1rem`))
-    (@foo)
-    (@bar `baz`)
-    (@media `(min-width: 1024px)`
-      (@rule `${root}` (padding `1.5rem`)))))
+(electron `blue` (color `#0000ff`))
+(electron `bg_green` (background-color `#00ff00`))
+
+(molecule `button`
+  (atom `label` (electrons `blue`)))
+
+(molecule `flag`
+  (atom `root` (electrons `red` `bg_green`))
+  (atom `label` (import `button` `label`))
+  (& `${root}`
+    (padding `1rem`)
+    (margin `0`))
+  (@ `foo`)
+  (@ `bar` `baz`)
+  (@ `media` `(min-width: 1024px)`
+    (& `${root}` (padding `1.5rem`))))
+
 "##;
 
         let result = parse(source);
-        println!("### {:?}", result);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().len(), 5);
     }
 }
